d48cef3e433864781cec72643dcc6475b8bc950f
nyan lin tun <devkonyan@gmail.com>
2020-01-31 01:47:55 +0900

    valodate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
   has_many :microposts
   validates FILL_IN, presence: true    # Replace FILL_IN with the right code.
-  validates FILL_IN, presence: true    # Replace FILL_IN with the right code.
 end
